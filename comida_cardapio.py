5773b6da792cfa3080b52f13f16e0883fcc9195d
SarasNunes <mikaellysara2021@gmail.com>
2025-05-12 21:56:58 -0300

    Add files via upload

---
+++ comida_cardapio.py
@@ -0,0 +1,23 @@
+opcao = 0
+while opcap != 5:
+    print ("Cardápio")
+    print ("1- Hamburguer")
+    print ("2- Pizza")
+    print ("3- Salada")
+    print ("4- Refrigerante")
+    print ("5- Sair")
+    opcao = int(input("Escolha uma opçaõ: "))
+    if opcao ==1:
+        print("Você escolheu hamburguer.")
+    elif opcao ==2:
+        print("Você escolheu pizza.")
+    elif opcao ==3:
+        print ("Você escolheu salada.")
+    elif opcao ==4:
+        print("Você escolheu refrigerante.")
+    elif opcao ==5:
+        print("Saindo do cardápio...")
+        break
+    else:
+        print ("Opção inválida. Tenta novamente")
+    
